e03422a7eba5c55ae2914f11af8f794eeae9a935
vishalgarg01 <vishalgarghi@gmail.com>
2020-09-24 12:21:17 +0530

    Configuring React

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById('root')); //root is in index.html. this line attach App class to root element
 registerServiceWorker();
